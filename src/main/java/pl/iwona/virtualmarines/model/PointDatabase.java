87c2bd34be231ea0ab8276b2443d8e62264544c3
Iwona007 <iwonka.adamkiewicz@gmail.com>
2021-12-02 23:08:45 +0100

    Add: model, apllication propertis, Pom.xml and run class

---
+++ src/main/java/pl/iwona/virtualmarines/model/PointDatabase.java
@@ -0,0 +1,96 @@
+package pl.iwona.virtualmarines.model;
+
+
+import javax.persistence.*;
+
+@Entity
+public class PointDatabase {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long Id;
+    private double y;
+    private double x;
+    private String name;
+    @Column(name="destination_x")
+    private double destinationX;
+    @Column(name="destination_Y")
+    private double destinationY;
+
+//    private String dateTime;
+
+    public PointDatabase() {
+    }
+
+//    public Point(double y, double x, String name, double destinationX, double destinationY, String dateTime) {
+//        this.y = y;
+//        this.x = x;
+//        this.name = name;
+//        this.destinationX = destinationX;
+//        this.destinationY = destinationY;
+//        this.dateTime = dateTime;
+//    }
+
+    public PointDatabase(double y, double x, String name, double destinationX, double destinationY) {
+        this.y = y;
+        this.x = x;
+        this.name = name;
+        this.destinationX = destinationX;
+        this.destinationY = destinationY;
+    }
+    public double getX() {
+        return x;
+    }
+
+    public void setX(double x) {
+        this.x = x;
+    }
+
+    public double getY() {
+        return y;
+    }
+
+    public void setY(double y) {
+        this.y = y;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public double getDestinationY() {
+        return destinationY;
+    }
+
+    public void setDestinationY(double destinationY) {
+        this.destinationY = destinationY;
+    }
+
+    public double getDestinationX() {
+        return destinationX;
+    }
+
+    public void setDestinationX(double destinationX) {
+        this.destinationX = destinationX;
+    }
+
+    public Long getId() {
+        return Id;
+    }
+
+    public void setId(Long id) {
+        Id = id;
+    }
+
+//    public String getDateTime() {
+//        return dateTime;
+//    }
+//
+//    public void setDateTime(String dateTime) {
+//        this.dateTime = dateTime;
+//    }
+}
